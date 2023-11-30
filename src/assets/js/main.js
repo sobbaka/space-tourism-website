e834a9c8b4c26e232ca0f7753751187a95c7c303
sobbaka <a.bagaev1989@gmail.com>
2023-11-30 17:40:30 +0400

    add: mobile menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,13 @@
 import "the-new-css-reset/css/reset.css";
 import '../styles/main.scss';
+
+
+const header = document.querySelector('.header')
+const burger = header.querySelector('.burger')
+const navigation = header.querySelector('.header__navigation')
+
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger--active')
+  navigation.classList.toggle('header__navigation--active')
+})
