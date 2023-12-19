cdb9f0a19de6672667162ec7cffe9213b7ecea3a
sobbaka <a.bagaev1989@gmail.com>
2023-12-19 19:11:04 +0400

    change menu links hover color

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,3 +11,10 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('burger--active')
   navigation.classList.toggle('header__navigation--active')
 })
+
+
+navigation.querySelectorAll('.header__link').forEach((link) => {
+  const path = window.location.pathname
+  if (link.href.includes(path) && path != '/') link.classList.add('header__link--active')
+  if (!link.href.includes(path) && link.classList.contains('header__link--active')) link.classList.remove('header__link--active')
+})
